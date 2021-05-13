2aaf792fa91da22f3946b703ec1ce642f09e68f2
Mart Rohel Sanchez <mart@symph.co>
2021-05-13 21:37:45 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import http from "http";
+
+import router from "./routes";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use("/", router);
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`The server is running on port ${port}`));
